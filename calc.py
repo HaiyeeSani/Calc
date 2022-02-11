2ca065ec627e90f98194151e2a25383bc9ec4d51
Anas-Tawalbeh <59394028+Anas-Tawalbeh@users.noreply.github.com>
2022-02-11 13:30:04 +0700

    Update calc.py file

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 """
 Program: Simple Calculator 
-Author: Anas Tawalbeh
+
 Simple calcuatro help the user calculate the basic 4 operations including:
 addition, subtraction, multiplication and division
 Significant constants
@@ -15,18 +15,70 @@ Significant constants
  4. The outputs are
          computation result
 """
+import sys
+number1= number2= result= 0
+operator=''
 
-number1=int(input("Insert the first value"))
-number2=int(input("Insert the second value"))
+def addition():
+        result=number1+number2
+        print(f"The sumation for the two values is equal to: ,{result}")
+def subtraction():
+        result=number1-number2
+        print(f"The sumation for the two values is equal to: ,{result}")
+def multiply():
+        result=number1*number2
+        print(f"The sumation for the two values is equal to: ,{result}")
+def divide():
+        result=number1/number2
+        print(f"The sumation for the two values is equal to: ,{result}")
+def again():
+        calc_again = input('''
+        Do you want to calculate again?
+        Please type Y for YES or N for NO.
+        ''')
+        if calc_again.upper() == 'Y':
+                main()
 
-addition=number1+number2
-print(f"The sumation for the two values is equal to: ,{addition}")
-
-subtraction=number1-number2
-print(f"The sumation for the two values is equal to: ,{subtraction}")
-
-multiplication=number1*number2
-print(f"The sumation for the two values is equal to: ,{multiplication}")
-
-division=number1/number2
-print(f"The sumation for the two values is equal to: ,{division}")
+        elif calc_again.upper() == 'N':
+                sys.exit('See you later.')
+        else:
+                again()
+def validate_input():
+    try:
+        number1=float(input("Insert the first value\n"))
+    except:
+        print("Your first input is not a number\n")
+        validate_input()
+def validate_second_input():
+    try:
+        number2=float(input("Insert the second value\n"))
+    except:
+        print("Your first second is not a number\n")
+        validate_input()
+def validate_operator():
+    operator = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+    if operator =='+':
+        addition()
+    if operator =='-':
+        subtraction()
+    if operator =='*':
+        multiply() 
+    if operator =='/':
+        divide()
+    else:
+        print("Invalid operator\n")
+        validate_operator()
+def main():
+        validate_input()
+        validate_second_input()
+        validate_operator()
+        again()
+        
+                
+main()
